4dbc47f9d423ee0dadb6b266040b40219e8b04d6
MegaSonic <adamkay90@gmail.com>
2017-07-06 11:45:46 -0400

    fix

--- enterprise/TObjects.js
+++ enterprise/TObjects.js
@@ -326,7 +326,7 @@ var TGhost = TObject.subclass('users.TGhost',
         this.distance.sub(this.object3D.position);
         this.distance.normalize();
 
-        this.distance.multiplyScalar(this.deltaTime);
+        //this.distance.multiplyScalar(this.deltaTime);
         //this.distance.multiplyScalar(this.chaseSpeed);
         console.log(this.distance.x + ', ' + this.distance.y + ', ' + this.distance.z);
         this.object3D.position.add(this.distance);
